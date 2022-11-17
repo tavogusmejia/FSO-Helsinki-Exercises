c356c99f31c1924b29eed73f6eb332585a598c8a
Gamut <tavogusmejia@gmail.com>
2022-11-17 05:54:39 -0500

    1.4: course information step4

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,25 +1,27 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
     <Header course={course}/>
-    <Content part={part1.name} exercises={part1.exercises}/>
-    <Content part={part2.name} exercises={part2.exercises}/>
-    <Content part={part3.name} exercises={part3.exercises}/>
-    <Total total={part1.exercises+part2.exercises+part3.exercises}/>  ...
+    <Content part={parts[0].name} exercises={parts[0].exercises}/>
+    <Content part={parts[1].name} exercises={parts[1].exercises}/>
+    <Content part={parts[2].name} exercises={parts[2].exercises}/>
+    <Total total={parts[0].exercises+parts[1].exercises+parts[2].exercises}/>  ...
     </div>
   )
 }
